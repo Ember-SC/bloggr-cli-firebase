9098b0d9be064a1091af3b99da372c0f86ab4d19
Scott Smith <scottnelsonsmith@gmail.com>
2014-06-11 22:11:22 -0700

    Removed console.log lines for debugging

--- app/routes/authors/new.js
+++ app/routes/authors/new.js
@@ -1,8 +1,6 @@
 export default Ember.Route.extend({
     model: function() {
-        window.console.log('authors new route before get');
         var store = this.get('store');
-        window.console.log('authors new route after get');
         return store.createRecord('author');
     }
 });
